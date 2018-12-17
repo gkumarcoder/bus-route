2e9a072bea43ac27b25eb0a33b6ff6357e939a88
Gaurav Kumar <gkumar4@outlook.com>
2018-12-17 22:03:03 +0530

    added test case

--- src/test/java/com/target/busroute/BusRouteApplicationTests.java
+++ src/test/java/com/target/busroute/BusRouteApplicationTests.java
@@ -1,15 +1,52 @@
 package com.target.busroute;
 
+import java.io.IOException;
+
+import org.junit.Ignore;
 import org.junit.Test;
 
-
-
+/**
+ * This class will test the bus-route application with different different boundary conditions.
+ * 
+ * @author gkumar3
+ */
 public class BusRouteApplicationTests {
+	
+	/**
+	 * In this case we are passing the correct route,stop and direction so,this test case will print the ETA (Estimated Arrival Time)
+	 * 
+	 * @throws IOException
+	 */
+	@Test
+	//@Ignore
+	public void testCase1() throws IOException {
+		String arg[] = { "5", "77PO", "north" };
+		BusRouteApplication.main(arg);
 
-    @Test
-    public void testCase() {
-    	//->
+	}
+	/**
+	 * In this case it will ask user to enter the direction name eg "Please enter correct direction name"
+	 * @throws IOException
+	 */
+	
+	@Test
+	@Ignore
+	public void testCase2() throws IOException {
+		String arg[] = { "5", "77PO", "" };
+		BusRouteApplication.main(arg);
 
-    }
+	}
+	/**
+	 * In this case it will ask user to enter the stop name eg "Please enter correct stop name"
+	 * @throws IOException
+	 */
+	@Test
+	@Ignore
+	public void testCase3() throws IOException {
+		String arg[] = { "5", "", "north" };
+		BusRouteApplication.main(arg);
 
+	}
+	
+  
 }
