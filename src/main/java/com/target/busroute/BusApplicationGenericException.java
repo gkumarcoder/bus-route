b2e3b949414a5192796d2afa865cc41b8117c226
Gaurav Kumar <gkumar4@outlook.com>
2018-12-18 00:31:37 +0530

    added custom exception class

---
+++ src/main/java/com/target/busroute/BusApplicationGenericException.java
@@ -0,0 +1,14 @@
+package com.target.busroute;
+
+public class BusApplicationGenericException extends RuntimeException  {
+	 
+		private static final long serialVersionUID = -7203418875731447925L;
+
+		public BusApplicationGenericException() {
+	    }
+
+	    public BusApplicationGenericException(String message) {
+	        super(message);
+	    }
+
+}
