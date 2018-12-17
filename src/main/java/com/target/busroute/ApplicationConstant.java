e36ce6bf09ca7fdcf0df222855dab7ec771c250a
Gaurav Kumar <gkumar4@outlook.com>
2018-12-17 21:50:47 +0530

    Update ApplicationConstant.java

--- src/main/java/com/target/busroute/ApplicationConstant.java
+++ src/main/java/com/target/busroute/ApplicationConstant.java
@@ -13,5 +13,8 @@ public class ApplicationConstant {
 	public static final String ERR_MSG = "Please enter the correct number of arguments.";
 	public static final String ENDPOINT="http://svc.metrotransit.org/";
 	public static final String ERR_IN_DIR=" is incorrect for this route";
+	public static final String ERR_IN_ROUTE = "Please enter correct route name";
+	public static final String ERR_IN_STOP = "Please enter correct stop name";
+	public static final String ERR_IN_DIRECTION = "Please enter correct direction name";
 
 }
