0a72fc09c3bcd7ca63323cee004ba8c1df139f21
Gaurav Kumar <gkumar4@outlook.com>
2018-12-17 21:49:25 +0530

    Update BusRouteApplication.java

--- src/main/java/com/target/busroute/BusRouteApplication.java
+++ src/main/java/com/target/busroute/BusRouteApplication.java
@@ -1,6 +1,9 @@
 package com.target.busroute;
 
 import java.io.*;
+import java.util.Optional;
+
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * @author gkumar3 This Application returns the time in minutes for a bus on
@@ -32,18 +35,30 @@ public class BusRouteApplication {
 			System.exit(-1);
 		}
 
-		String route = args[0];
-		String stop = args[1];
-		String direction = args[2];
+		String route = Optional.ofNullable(args[0]).orElse(StringUtils.EMPTY);
+		if (route == null || route.trim().length() == 0) {
+			System.out.println(ApplicationConstant.ERR_IN_ROUTE);
+			System.exit(-1);
+		}
+		String stop = Optional.ofNullable(args[1]).orElse(StringUtils.EMPTY);
 
-		//Convert direction id
+		if (stop == null || stop.trim().length() == 0) {
+			System.out.println(ApplicationConstant.ERR_IN_STOP);
+			System.exit(-1);
+		}
+		String direction = Optional.ofNullable(args[2]).orElse(StringUtils.EMPTY);
+		if (direction == null || direction.trim().length() == 0) {
+			System.out.println(ApplicationConstant.ERR_IN_DIRECTION);
+			System.exit(-1);
+		}
+		// Convert direction id
 		dir = BusRouteInfoService.getDirection(direction);
-		if(Direction.ERROR.getDirection().equals(dir))
-		{
+		if (Direction.ERROR.getDirection().equals(dir)) {
 			System.out.println(dir);
+			System.exit(-1);
 		}
 
-		//Verifies the route ID for the specific route that was entered and return the ID
+		// Verifies the route ID for the specific route that was entered and return the ID
 		routeID = BusRouteInfoService.getBusRouteId(ApplicationConstant.ENDPOINT + "NexTrip/Routes?format=json",
 				"Description", "Route", route);
 		if (routeID == -1) {
@@ -51,7 +66,7 @@ public class BusRouteApplication {
 			System.exit(-1);
 		}
 
-		//Verifies the directionID for the specific direction and if found matched direction return the valid ID
+		// Verifies the directionID for the specific direction and if found matched direction return the valid ID
 		directionID = BusRouteInfoService.getBusRouteDirection(
 				ApplicationConstant.ENDPOINT + "NexTrip/Directions/" + routeID + "?format=json", "Text", "Value", dir);
 		if (directionID == -1) {
@@ -68,16 +83,18 @@ public class BusRouteApplication {
 			System.exit(-1);
 		}
 
-		//Verifies the timeStamp String and if found matched timeStamp return the departureTime.
+		// Verifies the timeStamp String and if found matched timeStamp return the
+		// departureTime.
 		timeStamp = BusRouteInfoService.getBusRouteTimeStamp(
 				ApplicationConstant.ENDPOINT + "NexTrip/" + routeID + "/" + directionID + "/" + stopID + "?format=json",
 				"RouteDirection", "DepartureTime", timeStamp);
 		if (timeStamp.equals("")) {
-		//The specification says that if the last bus of the day has already left to not return anything. So I exit clean here.
+			// The specification says that if the last bus of the day has already left to
+			// not return anything. So I exit clean here.
 			System.exit(0);
 		}
 
-		//compute the time and print the result
+		// compute the time and print the result
 		BusRouteInfoService.computeTime(timeStamp);
 	}
 
